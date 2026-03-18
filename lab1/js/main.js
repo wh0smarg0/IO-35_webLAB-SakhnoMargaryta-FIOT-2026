a7193862c4b00494de2708405321b0244a4a2f63
wh0smarg0 <wh0smarg0@users.noreply.github.com>
2026-03-18 11:05:17 +0000

    deploy: 86c42b3219d1d3ed6ee8273599a7a98ea5d45802

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -3,21 +3,65 @@ document.addEventListener('DOMContentLoaded', () => {
     const sharpBubble = document.querySelector('.hero-bg-sharp-bubble');
 
     if (hero && sharpBubble) {
+        let mx = 0;
+        let my = 0;
+        
+        let bx = 0;
+        let by = 0;
+        
+        const lerpFactor = 0.08; 
+        
+        let isVisible = false;
+
+        let isMoving = false;
+
+        // --- ВІДСТЕЖЕННЯ МИШІ ---
         hero.addEventListener('mousemove', (e) => {
-            const rect = hero.getBoundingClientRect();
-            const heroWidth = rect.width;
-            const heroHeight = rect.height;
-
-            const mouseXPercent = ((e.clientX - rect.left) / heroWidth) * 100;
-            const mouseYPercent = ((e.clientY - rect.top) / heroHeight) * 100;
-
-            const bubbleSize = '15vw';
-
-            sharpBubble.style.clipPath = `circle(${bubbleSize} at ${mouseXPercent}% ${mouseYPercent}%)`;
+            mx = e.clientX;
+            my = e.clientY;
+            
+            if (!isVisible) {
+                isVisible = true;
+                sharpBubble.style.setProperty('--mask-opacity', '1.0');
+            }
         });
 
         hero.addEventListener('mouseleave', () => {
-            sharpBubble.style.clipPath = 'circle(0% at 50% 50%)';
+            isVisible = false;
+            mx = -100;
+            my = -100; 
+            sharpBubble.style.setProperty('--mask-opacity', '0.0');
         });
+
+        // --- ГОЛОВНИЙ ЦИКЛ АНІМАЦІЇ  ---
+        function updateLiquidFocus() {
+            if (isVisible) {
+
+                const rect = hero.getBoundingClientRect();
+                const heroWidth = rect.width;
+                const heroHeight = rect.height;
+
+                bx += (mx - bx) * lerpFactor;
+                by += (my - by) * lerpFactor;
+
+                const vx = mx - bx;
+                const vy = my - by;
+                const velocity = Math.min(100, Math.sqrt(vx*vx + vy*vy));
+                
+                const distortionPower = 0.02;
+                const wobbleDistortion = 1.0 + (velocity * distortionPower);
+                
+                const maskXPercent = ((bx - rect.left) / heroWidth) * 100;
+                const maskYPercent = ((by - rect.top) / heroHeight) * 100;
+
+                sharpBubble.style.setProperty('--mask-x', `${maskXPercent}%`);
+                sharpBubble.style.setProperty('--mask-y', `${maskYPercent}%`);
+                sharpBubble.style.setProperty('--mask-wobble-w', wobbleDistortion.toFixed(3));
+            }
+
+            requestAnimationFrame(updateLiquidFocus);
+        }
+
+        updateLiquidFocus();
     }
 });
