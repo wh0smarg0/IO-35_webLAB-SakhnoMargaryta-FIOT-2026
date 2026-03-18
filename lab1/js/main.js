af2c6a020ee00e87580c7308717789ff33826b8b
wh0smarg0 <wh0smarg0@users.noreply.github.com>
2026-03-18 10:56:50 +0000

    deploy: d7650c55c2e6d554a585afd634a954407c48dc2b

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('burger-menu');
-    const navLinks = document.getElementById('nav-links');
+    const hero = document.querySelector('.hero');
+    const sharpBubble = document.querySelector('.hero-bg-sharp-bubble');
 
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
+    if (hero && sharpBubble) {
+        hero.addEventListener('mousemove', (e) => {
+            const rect = hero.getBoundingClientRect();
+            const heroWidth = rect.width;
+            const heroHeight = rect.height;
+
+            const mouseXPercent = ((e.clientX - rect.left) / heroWidth) * 100;
+            const mouseYPercent = ((e.clientY - rect.top) / heroHeight) * 100;
+
+            const bubbleSize = '15vw';
+
+            sharpBubble.style.clipPath = `circle(${bubbleSize} at ${mouseXPercent}% ${mouseYPercent}%)`;
+        });
+
+        hero.addEventListener('mouseleave', () => {
+            sharpBubble.style.clipPath = 'circle(0% at 50% 50%)';
+        });
+    }
 });
